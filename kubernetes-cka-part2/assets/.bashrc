a6ca5eed6c2dd48b214dfea9e075960e1df5f41b
djkormo <djkormo@gmail.com>
2022-05-28 14:28:30 +0200

    correcting install.sh

--- kubernetes-cka-part2/assets/.bashrc
+++ kubernetes-cka-part2/assets/.bashrc
@@ -1,6 +1,5 @@
 free_time()
 {
-    #awk "{print $1}" /proc/uptime
     uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print 60-$2" "$3 }'
 }
 git_branch() {
@@ -9,4 +8,8 @@ git_branch() {
 GREEN="\[$(tput setaf 2)\]"
 RESET="\[$(tput sgr0)\]"
 export PS1="$(free_time) \[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
-export do=" --dry-run=client -o yaml"
+export do=" --dry-run=client -o yaml "
+alias k='kubectl '
+alias kubens='kubectl config set-context --current --namespace '
+alias kubectx='kubectl config use-context '
+alias kubegc='kubectl config get-contexts '
--- kubernetes-cka-part2/install.sh
+++ kubernetes-cka-part2/install.sh
@@ -21,34 +21,17 @@ free_time()
 source ~/.bashrc
 
 
-apt-get update >> /var/log/install
-apt-get install dnsutils -y >> /var/log/install
-apt-get install kubeadm=1.19.0-00 kubectl=1.19.0-00 kubelet=1.19.0-00 -y  >> /var/log/install
-systemctl restart kubelet  >> /var/log/install
-kubeadm upgrade apply v1.19.0 -y  >> /var/log/install
-systemctl restart kubelet  >> /var/log/install
-
-echo "Upgrading cluster"
-upgrade.sh
-echo "done" >> /opt/.clusterupgraded
-date >> /opt/.clusterupgraded
-
-echo "Upgrading node"
-ssh root@[[HOST2_IP]] "apt-get update && apt-get install kubeadm=1.19.0-00 kubelet=1.19.0-00 -y && systemctl restart kubelet"  >> /var/log/install
-
-echo "done" >> /opt/.nodeupgraded
-date >> /opt/.nodeupgraded
-
-
 echo "done" >> /opt/.nodeupgraded
 date >> /opt/.nodeupgraded
 
 (
   set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
-  "$KREW" install krew
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
 )
 
 
@@ -56,6 +39,9 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 
 ## Installing metrics server 
-git clone https://github.com/vocon-it/metrics-server >>/var/log/step1-background.log
-kubectl apply -f ./metrics-server/deploy/1.8+/ >>/var/log/step1-background.log
+# Documented here 
+# https://thospfuller.com/2020/11/29/easy-kubernetes-metrics-server-install-in-minikube-in-five-steps/
+
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f https://gist.githubusercontent.com/thospfuller/d0d918e0b9fdb719a34d3d355b0886bb/raw/63fefb2d1cf31563db04c60f036d6661b5ec5ff2/components.yaml
 
