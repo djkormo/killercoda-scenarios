95de1dda604f7ea35dc26144a7a5461ddf5b58ab
djkormo <djkormo@gmail.com>
2022-05-28 15:56:11 +0200

    changing k8s versio to 1.23

--- kubernetes-cka-part1/install.sh
+++ kubernetes-cka-part1/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo off
-apt-get update && apt-get install kubeadm=1.23.1-00 kubelet=1.23.1-00 -y && systemctl restart kubelet"  >> /var/log/install
+apt-get update && apt-get install kubeadm=1.23.1-00 kubelet=1.23.1-00 -y && systemctl restart kubelet  >> /var/log/install
 systemctl restart kubelet >> /var/log/install
 
 ssh root@[[host2]] "apt-get update && apt-get install kubeadm=1.23.1-00 kubelet=1.23.1-00 -y && systemctl restart kubelet"  >> /var/log/install
--- kubernetes-cka-part1/step0-answer.md
+++ kubernetes-cka-part1/step0-answer.md
@@ -1,10 +1,10 @@
-To continue you should have 1.18 Kubernetes cluster with two nodes (ready)
+To continue you should have 1.23 Kubernetes cluster with two nodes (ready)
 
 An example:
 
 <pre>
 kubectl get nodes
 NAME     STATUS   ROLES    AGE    VERSION
-master   Ready    master   104s   v1.18.0
-node01   Ready    <none>   66s    v1.18.0
+master   Ready    master   104s   v1.23.0
+node01   Ready    <none>   66s    v1.23.0
 </pre>
