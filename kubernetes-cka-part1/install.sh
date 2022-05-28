210f3bbc82f4ba037329bf4cdedfd539a7e65809
djkormo <djkormo@gmail.com>
2022-05-28 15:47:11 +0200

    initial version of part1

---
+++ kubernetes-cka-part1/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo off
+apt-get update && apt-get install kubeadm=1.23.1-00 kubelet=1.23.1-00 -y && systemctl restart kubelet"  >> /var/log/install
+systemctl restart kubelet >> /var/log/install
+
+ssh root@[[host2]] "apt-get update && apt-get install kubeadm=1.23.1-00 kubelet=1.23.1-00 -y && systemctl restart kubelet"  >> /var/log/install
+
+# Start Kubernetes
+echo "Starting cluster"
+
+source ~/.bashrc
+
+echo "done" >> /opt/.clusterstarted
